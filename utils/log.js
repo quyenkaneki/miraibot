ba495a24c053eba33f937d36352279bac195e2c0
quyenkaneki <95172923+quyenkaneki@users.noreply.github.com>
2022-02-25 09:55:08 +0700

    Update log.js

--- utils/log.js
+++ utils/log.js
@@ -9,7 +9,7 @@ module.exports = (data, option) => {
 			console.log(chalk.bold.hex("#FF0000")('[ ❗ Lỗi rồi ] » ') + data);
 			break;
 		default:
-			console.log(chalk.bold.hex("#FF4500")(`${option} » `) + data);
+			console.log(chalk.bold.hex("33FFCC")(`${option} » `) + data);
 			break;
 	}
 }
@@ -17,10 +17,10 @@ module.exports = (data, option) => {
 module.exports.loader = (data, option) => {
 	switch (option) {
 		case "warn":
-			console.log(chalk.bold.hex("#FF0000")('» •KANEKI• « ') + data);
+			console.log(chalk.bold.hex("#FFFF00")('» •KANEKI• « ') + data);
 			break;
 		case "error":
-			console.log(chalk.bold.hex("#FFFF00")('» •KANEKI• « ') + data);
+			console.log(chalk.bold.hex("#FF0000")('» •KANEKI• « ') + data);
 			break;
 		default:
 			console.log(chalk.bold.hex("#00FFFF")('» •KANEKI• « ') + data);
