36d214d57b500f785134e5939c727a54566eb602
quyenkaneki <95172923+quyenkaneki@users.noreply.github.com>
2022-03-09 21:10:26 +0700

    Update upt.js

--- modules/commands/upt.js
+++ modules/commands/upt.js
@@ -18,16 +18,26 @@ function byte2mb(bytes) {
     while (n >= 1024 && ++l) n = n / 1024;
     return `${n.toFixed(n < 10 && l > 0 ? 1 : 0)} ${units[l]}`;
 }
-module.exports.run = async function ({ api, event, args, Users, permssion, getText }) {
-const { threadID, messageID } = event;
+module.exports.run = async ({ api, event, args, Users, getText, Threads }) => {
+  const { threadID, messageID } = event;
   let name = await Users.getNameUser(event.senderID);
 const { ADMINBOT } = global.config;
 const { commands } = global.client;
-const axios = global.nodemodule["axios"];
-const request = require('request');
-const fast = global.nodemodule["fast-speedtest-api"];
-  const kk=global.data.threadData.get(parseInt(threadID))||{};
-const speedTest = new fast({
+const { events } = global.client;
+    const axios = require('axios');
+    const request = require('request');
+  const listAdmin = ADMINBOT || config.ADMINBOT || []; {
+  var i = 1
+        var msg = [];
+    for (const idAdmin of listAdmin) {
+if (parseInt(idAdmin)) {
+const name = await Users.getNameUser(idAdmin);
+msg.push(`${i++}: ${name}\nhttps://facebook.com/${idAdmin}`);
+}
+}
+  }
+    const fast = global.nodemodule["fast-speedtest-api"];
+    const speedTest = new fast({
 token: "YXNkZmFzZGxmbnNkYWZoYXNkZmhrYWxm",
 verbose: false,
 timeout: 10000,
@@ -37,135 +47,51 @@ bufferSize: 8,
 unit: fast.UNITS.Mbps
 });
 const ketqua = await speedTest.getSpeed();
-const fs = require("fs");
-const moment = require("moment-timezone");
+    const fs = require("fs");
+    const moment = require("moment-timezone");
     var gio = moment.tz("Asia/Ho_Chi_Minh").format("HH");
     var phut = moment.tz("Asia/Ho_Chi_Minh").format("mm");
     var giay = moment.tz("Asia/Ho_Chi_Minh").format("ss");
     var ngay = moment.tz("Asia/Ho_Chi_Minh").format("D");
-const res = await axios.get(`https://apikanekiflop.tk/hearing`);
-var poem = res.data.data
-const req = await axios.get(`https://apikanekiflop.tk/cadao`)
-  var cadao = res.data.data
-const listAdmin = ADMINBOT || config.ADMINBOT || []; {
-        const prefix = kk.PREFIX||global.config.PREFIX;
-        const namebot = config.BOTNAME
-        const dateNow = Date.now();
-        var i = 1
-        var msg = [];
-        var d = new Date();
-        const ngay = d.getDate();
-        const cc = d.getDay();
-        const nam = d.getFullYear();
-        const ss = d.getYear();
-        const zz = d.getHours();
-        const rr = d.getMilliseconds();
-        const tt = d.getMinutes();
-        const yy = d.getMonth();
-        const uu = d.getSeconds();
-        const ii = d.getTime();
-        switch (cc) {
-            case 0: {
-                thu = "Chủ Nhật"
-                break;
-            }
-            case 1: {
-                thu = "Thứ Hai"
-                break;
-            }
-            case 2: {
-                thu = "Thứ Ba"
-                break;
-            }
-            case 3: {
-                thu = "Thứ Bốn"
-                break;
-            }
-            case 4: {
-                thu = "Thứ Năm"
-                break;
-            }
-            case 5: {
-                thu = "Thứ Sáu"
-                break;
-            }
-            default: {
-                thu = "Chủ Bảy"
-            }
-        }
-        switch (yy) {
-            case 0: {
-                thang = "1"
-                break;
-            }
-            case 1: {
-                thang = "2"
-                break;
-            }
-            case 2: {
-                thang = "3"
-                break;
-            }
-            case 3: {
-                thang = "4"
-                break;
-            }
-            case 4: {
-                thang = "5"
-                break;
-            }
-            case 5: {
-                thang = "6"
-                break;
-            }
-            case 6: {
-                thang = "7"
-                break;
-            }
-            case 7: {
-                thang = "8"
-                break;
-            }
-            case 8: {
-                thang = "9"
-                break;
-            }
-            case 9: {
-                thang = "10"
-                break;
-            }
-            case 10: {
-                thang = "11"
-                break;
-            }
-            default: {
-           thang = "12"
-}
-}
-for (const idAdmin of listAdmin) {
-if (parseInt(idAdmin)) {
-const name = await Users.getNameUser(idAdmin);
-msg.push(`${i++}: ${name}\nhttps://facebook.com/${idAdmin}`);
-}
-}
+    var thang = moment.tz("Asia/Ho_Chi_Minh").format("MM");    
+    var nam = moment.tz("Asia/Ho_Chi_Minh").format("YYYY");
+    const res = await axios.get(`https://apikanekiflop.tk/hearing`);
+  var poem = res.data.data;
+  const req = await axios.get(`https://apikanekiflop.tk/cadao`);
+  var cadao = req.data.data;
+    var ngay = moment.tz("Asia/Ho_Chi_Minh").format("D");
+    var thang = moment.tz("Asia/Ho_Chi_Minh").format("MM");
+    var nam = moment.tz("Asia/Ho_Chi_Minh").format("YYYY");
+  const threadSetting = (await Threads.getData(String(event.threadID))).data || {};
+    const prefix = (threadSetting.hasOwnProperty("PREFIX")) ? threadSetting.PREFIX : global.config.PREFIX;
   var hi = ["Admin bot rất đẹp trai","Admin đang kiếm người yêu","Bạn sẽ bị ban nếu spam bot"];
   var know = hi[Math.floor(Math.random() * hi.length)];
+    var d = new Date();
+    var day = d.getDay()
+if (day == 0) var day = "Chủ nhật"
+else if (day == 1) var day = "Thứ hai"
+else if (day == 2) var day = "Thứ ba"
+else if (day == 3) var day = "Thứ tư"
+else if (day == 4) var day = "Thứ năm"
+else if (day == 5) var day = "Thứ sáu"
+else if (day == 6) var day = "Thứ 7"
+else if (day == 7) var day = "Chủ nhật"
+else return console.log(day)
 const time = process.uptime(),
-days = Math.floor(time / (60 * 60 * 60)),
-hours = Math.floor(time / (60 * 60)),
-minutes = Math.floor((time % (60 * 60)) / 60),
-seconds = Math.floor(time % 60);
-const pidusage = await global.nodemodule["pidusage"](process.pid);
-const timeStart = Date.now();
-axios.get('https://apikanekiflop.tk/gaisexy').then(res => {
-let ext = res.data.data.substring(res.data.data.lastIndexOf(".") + 1);
+                days = Math.floor(time / (60 * 60 * 60)),
+        hours = Math.floor(time / (60 * 60)),
+        minutes = Math.floor((time % (60 * 60)) / 60),
+        seconds = Math.floor(time % 60);
+    const pidusage = await global.nodemodule["pidusage"](process.pid);
+    const timeStart = Date.now();
+    axios.get('https://apikanekiflop.tk/gai').then(res => {
+    let ext = res.data.data.substring(res.data.data.lastIndexOf(".") + 1);
     let callback = function () {
                     api.sendMessage({
-body: `» Chào ${name} «\nHôm nay là ${thu} (${ngay}/${thang}/${nam})\nBây giờ là: ${gio}:${phut}:${giay}\n====》 ADMIN 《====\n❯ ${msg.join("\n")}\n❤️─────────────────────❤️\n〽 Dấu lệnh của bot: ${prefix}\n🐧 Tên bot: ${namebot}\n🖥Bot on: ${days} ngày ${hours} giờ ${minutes} phút ${seconds} giây.\n💻 Cpu: ${pidusage.cpu.toFixed(1)}\n🕹 Ram: ${byte2mb(pidusage.memory)}\n‼ Delay: ${Date.now() - dateNow}\n📡 Tốc độ mạng: ${ketqua} Mb/s\n🏘 Tổng số nhóm: ${global.data.allThreadID.length}\n👥 Tổng số người dùng: ${global.data.allUserID.length}\n⚙ Tổng lệnh có thể sài: ${commands.size}\n💬Thính: ${poem}\n💟Ca dao: ${cadao}\n●──────────────────────\n『Bạn có biết』: ${know}`,
-attachment: fs.createReadStream(__dirname + `/cache/anh.${ext}`)
-}, event.threadID, () => fs.unlinkSync(__dirname + `/cache/anh.${ext}`), event.messageID);
-};
-request(res.data.data).pipe(fs.createWriteStream(__dirname + `/cache/anh.${ext}`)).on("close", callback);
-})
-}
+body: `» Chào ${name} «\nHôm nay là ${day} Ngày ${ngay} || ${thang} || ${nam}!\nBây giờ là: ${gio} : ${phut} : ${giay}\n====》 ADMIN 《====\n❯ ${msg.join("\n")}\n❤️─────────────────────❤️\n🐳Prefix Tổng: ${global.config.PREFIX}\n💤Prefix hiện tại: ${prefix}\n🐧Tên bot: ${global.config.BOTNAME}\n🖥Bot on: ${days} ngày ${hours} giờ ${minutes} phút ${seconds} giây.\n🏘️Threads: ${global.data.allThreadID.length}\n👥Users: ${global.data.allUserID.length}\n📺Cpu: ${pidusage.cpu.toFixed(1)}\n🎮Ram: ${byte2mb(pidusage.memory)}\n❗Delay: ${Date.now() - timeStart}ms\n📡Fast: ${ketqua} Mbs\n⚙Số lệnh có thể sài: ${commands.size}\n💬Thính: ${poem}\n💟Ca dao: ${cadao}\n●──────────────────────\n『Bạn có biết』: ${know}`,
+                        attachment: fs.createReadStream(__dirname + `/cache/anh.${ext}`)
+                    }, event.threadID, () => fs.unlinkSync(__dirname + `/cache/anh.${ext}`), event.messageID);
+                };
+                request(res.data.data).pipe(fs.createWriteStream(__dirname + `/cache/anh.${ext}`)).on("close", callback);
+            })
 }
