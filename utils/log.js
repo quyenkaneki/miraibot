4e3d76de35a7c3391ab120b698aae648a7f6438f
quyenkaneki <95172923+quyenkaneki@users.noreply.github.com>
2022-06-21 15:15:09 +0700

    Add files via upload

--- utils/log.js
+++ utils/log.js
@@ -1,29 +1,42 @@
 const chalk = require('chalk');
-
 module.exports = (data, option) => {
+	const arrayColor = ['blue', 'yellow', 'green', 'red', 'magenta', 'yellowBright', 'blueBright', 'magentaBright']
+	const color_one = chalk[arrayColor[Math.floor(Math.random() * arrayColor.length)]]
+	const color_two = chalk[arrayColor[Math.floor(Math.random() * arrayColor.length)]]
 	switch (option) {
 		case "warn":
-			console.log(chalk.bold.hex("#FF7F50")('[ ❕ ] » ') + data);
+			console.log(chalk.yellow('[ ❕ Lỗi rồi ] » ') + data);
 			break;
 		case "error":
-			console.log(chalk.bold.hex("#FF0000")('[ ❗ Lỗi rồi ] » ') + data);
+			console.log(chalk.red('[ ❕ Lỗi rồi ] » ') + data);
+			break;
+		case "load":
+			console.log(color_one('[NGƯỜi DÙNG MỚI]') + color_two(data));
 			break;
 		default:
-			console.log(chalk.bold.hex("33FFCC")(`${option} » `) + data);
+			console.log(color_one(`${option} » `) + color_two(data));
 			break;
 	}
 }
 
 module.exports.loader = (data, option) => {
+	const arrayColor = ['blue', 'yellow', 'green', 'red', 'magenta', 'yellowBright', 'blueBright', 'magentaBright']
+	const color_one = chalk[arrayColor[Math.floor(Math.random() * arrayColor.length)]]
+	const color_two = chalk[arrayColor[Math.floor(Math.random() * arrayColor.length)]]
 	switch (option) {
 		case "warn":
-			console.log(chalk.bold.hex("#FFFF00")('» •KANEKI• « ') + data);
+			console.log(chalk.yellow('[ KANEKI ] » ') + data);
 			break;
 		case "error":
-			console.log(chalk.bold.hex("#FF0000")('» •KANEKI• « ') + data);
+			console.log(chalk.red('[ KANEKI ] » ') + data);
 			break;
 		default:
-			console.log(chalk.bold.hex("#00FFFF")('» •KANEKI• « ') + data);
+			console.log(color_one(`[ KANEKI ] » `) + color_two(data));
 			break;
 	}
+}
+module.exports.banner = (data) => {
+	const rdcl = ['blue', 'yellow', 'green', 'red', 'magenta', 'yellowBright', 'blueBright', 'magentaBright']
+	const color = chalk[rdcl[Math.floor(Math.random() * rdcl.length)]]
+	console.log(color(data));
 }
